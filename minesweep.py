71281efcb808a9857773cf2c499f884d2d05edf9
Ledenel <ledenelintelli@gmail.com>
2020-08-14 01:49:02 +0800

    add calculation for all (slow and no all solution point to find minima.)

--- minesweep.py
+++ minesweep.py
@@ -23,14 +23,19 @@ def gen_map(w, h, mines):
 def collect_adj(array: np.ndarray):
     collected = np.empty_like(array, dtype=object)
     for (i, j), _ in np.ndenumerate(array):
-        adj_mask = np.full_like(array, False)
-        adj_mask[max(i - 1, 0):i + 2, max(j - 1, 0):j + 2] = True
-        adj_mask[i, j] = False
-        collected[i, j] = list(array[adj_mask])
+        # adj_mask = np.full_like(array, False)
+        sliced = array[max(i - 1, 0):i + 2, max(j - 1, 0):j + 2]
+        mi, mj = sliced.shape
+        si, sj = mi - 2, mj - 2
+        collected[i, j] = list(sliced[i, j] for i in range(mi) for j in range(mj) if not (i == si and j == sj))
     return collected
 
 
-w, h, mines = [st.sidebar.number_input(name, value=v) for name, v in zip("w,h,mines".split(","), [10, 10, 30])]
+w, h, mines = [
+    st.sidebar.number_input(name, value=v) for name, v in zip(
+        "w,h,mines".split(","),
+        [3,3,4]
+    )]
 
 has_mine = gen_map(w, h, mines)
 numbers = np.vectorize(sum)(collect_adj(has_mine))
@@ -50,11 +55,11 @@ def render_text(is_mine, number, opening):
 
 i_s, j_s = np.indices((h, w))
 
-now : np.ndarray = st.cache(allow_output_mutation=True)(lambda: np.full((h, w), False))()
+now: np.ndarray = st.cache(allow_output_mutation=True)(lambda: np.full((h, w), False))()
 texts = np.vectorize(render_text)(has_mine, numbers, now)
 
 rect_sources = ColumnDataSource(data={
-    "now_alpha": (~now).astype(np.float).flatten().clip(0.2,0.8),
+    "now_alpha": (~now).astype(np.float).flatten().clip(0.2, 0.8),
     "x": j_s.flatten(),
     "y": i_s.flatten(),
 })
@@ -83,4 +88,34 @@ if toggling_index is not None:
     st.write(flat_now)
     now[:] = flat_now.reshape(now.shape)
     st.write(now)
-prob_symbols = np.array([[sp.Symbol(f"P_{{{i},{j}}}") for j in range(w)] for i in range(h)])
+
+prob_symbols = np.array([[sp.Symbol(f"P_{{{i},{j}}}", real=True) for j in range(w)] for i in range(h)])
+
+opened_is_known = prob_symbols - has_mine.astype(int)
+opened_is_known = opened_is_known[now]
+
+around_numbers = np.vectorize(sum)(collect_adj(prob_symbols)) - numbers
+around_numbers = around_numbers[now]
+
+all_bombs = np.array([sum(prob_symbols.flatten()) - mines])
+
+
+def entropy(x):
+    return -x * sp.log(x)
+
+
+entropy_energy = sum(np.vectorize(entropy)(prob_symbols.flatten()))
+
+eqs = np.concatenate([opened_is_known, around_numbers, all_bombs])
+
+lambdas = np.array([sp.Symbol(f"\\lambda_{i}", real=True) for i in range(len(eqs))])
+
+L = entropy_energy + sum(lambdas * eqs)
+variables = np.concatenate([prob_symbols.flatten(), lambdas])
+gradL = [sp.diff(L, x) for x in variables]
+
+stationary_points = sp.solve(gradL, variables.tolist(), dict=True)
+# np.ones_like(variables) - 0.5,
+max_point = max((p for p in stationary_points), key=lambda p: L.subs(p))
+
+st.write(max_point)
